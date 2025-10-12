f0074f6b3bf42d327b39506be7640355b6b074aa
Dharmendra Thakur <dharmendrathakur.dev@gmail.com>
2025-10-12 16:44:48 +0545

    changes

--- src/app/(Home)/page.jsx
+++ src/app/(Home)/page.jsx
@@ -4,6 +4,18 @@ import Hero from './components/Hero'; // Server Component
 import LoggedInHome from './LoggedInHome'; // Client Component
 import ClientAuthWrapper from './ClientAuthWrapper/ClientAuthWrapper'; // NEW
 
-export default function Page() {
-  return <ClientAuthWrapper fallback={<Hero />} />;
+export default async function Page() {
+    const response = await fetch(`${process.env.BASE_API}/HomeContent/GetPagedHomeContentList?pageIndex=1&pageSize=10`, {
+  });
+
+  if (!response.ok) {
+    console.error("Failed to fetch home content");
+    return <div>Failed to load content</div>;
+  }
+
+  const data = await response.json();
+  const baseContent = process.env.BASE_CONTENT;
+  return <ClientAuthWrapper fallback={<Hero  data={data} baseContent={baseContent}/>} />;
 }
+
+
